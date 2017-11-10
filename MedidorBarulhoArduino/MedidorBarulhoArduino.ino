5558e432dde740a166dd3d37344207e6ec85c38a
Erika Pessôa <erika.pessoa@gmail.com>
2017-11-10 16:45:25 -0300

    Ainda com problema no Loop

--- MedidorBarulhoArduino/MedidorBarulhoArduino.ino
+++ MedidorBarulhoArduino/MedidorBarulhoArduino.ino
@@ -1,18 +1,16 @@
 #include <MeetAndroid.h>
 MeetAndroid meetAndroid;
 
-
-
 int sSensorAnalPin = A15; //porta analógica ligado ao sensor de som
 int sSensorActualValue = 0;  //variável que guarda o valor que o sensor de som analógico esta captando
 int sSensorLimit = 0; //valor que define o limite de barulho que pode ser feito no ambiente
 int sSensorTimeMesuring = 0; //indica o tempo que o sensor de som irá ficar medindo o som ambiente
 int androidValues[] = {0,0}; //o primeiro valor é o limite e o segundo o tempo de medição
 //int sSensorCalibration = 0; //valor que define a calibração do microfone
-const String HALF_VALUE = "half_value";
-const String EXCEEDED_VALUE = "exceeded_value";
-String FINISH = "finish";
-const String STOP = "stop";
+const String HALF_VALUE = "HALF_VALUE";
+const String EXCEEDED_VALUE = "EXCEEDED_VALUE";
+const String FINISH = "FINISH";
+const String STOP = "STOP";
 
 
 void setup()
@@ -25,8 +23,8 @@ void setup()
   sSensorLimit = 0;
   sSensorTimeMesuring = 0;
   //sSensorCalibration = 0; 
-  androidValues[0] = 60;
-  androidValues[1] = 2;
+  androidValues[0] = 0;
+  androidValues[1] = 0;
   //Registrando a função que será chamada pelo Android para iniciar a medição.
   meetAndroid.registerFunction(iniciarMedicao, 'I');
   //Registrando a função que será chamada pelo Android para parar a medição antes do tempo acabar.
@@ -41,9 +39,10 @@ void loop(){
 
 void iniciarMedicao() {
 
-  Serial.println("Entrou em iniciarMedicao!!!");
+  //Serial.println("Entrou em iniciarMedicao!!!");
+  meetAndroid.send("LOG: Entrou em iniciarMedicao!!!");
   
-  int tempoMilisegundos = 60.000;
+  long tempoMiliSegundos = 60000;
   int segundos = 0;
   
   setup();
@@ -52,21 +51,35 @@ void iniciarMedicao() {
   sSensorLimit = androidValues[0];
   sSensorTimeMesuring = androidValues[1];
 
-  tempoMilisegundos = sSensorTimeMesuring * 60.000; //o valor do tempo vem em minutos
+  meetAndroid.send("LOG: sSensorLimit:  ");
+  meetAndroid.send(sSensorLimit);
+  meetAndroid.send("LOG: sSensorTimeMesuring:  ");
+  meetAndroid.send(sSensorTimeMesuring);
   
-  for (int i = 0; i <= tempoMilisegundos; i++) {
+  tempoMiliSegundos = sSensorTimeMesuring * 60000; //o valor do tempo vem em minutos
+
+  
+  meetAndroid.send("LOG: Tempo em milisegundos:  ");
+  meetAndroid.send(tempoMiliSegundos);
+  
+  for (long i = 0; i <= tempoMiliSegundos; i++) {
     segundos = i % 1000;
     
     if (segundos == 0) { //para ler apenas a cada segundo
-      Serial.print("SEGUNDO: ");
-      Serial.println(segundos);
+      //Serial.print("SEGUNDO: ");
+      //Serial.println(segundos);
       sSensorActualValue = analogRead(sSensorAnalPin);  
       verificaLimite();
     }
+    
+    //meetAndroid.send("LOG: Valor de i =  ");
+    //meetAndroid.send(i);
   }
+  
   //envia mensagem para o Android avisando que acabou a medição
-  //meetAndroid.send(FINISH);
-  Serial.print("TERMINOU!!!");
+  meetAndroid.send("FINISH");
+  //Serial.print("TERMINOU!!!");
+  
 }
 
 
@@ -74,18 +87,21 @@ void verificaLimite() {
 
   int metade = sSensorLimit / 2;
 
+  meetAndroid.send("LOG: Leitura atual do sensor: ");
   meetAndroid.send(sSensorActualValue); //envia para o android a leitura atual do sensor
-  Serial.print("VAlOR DO SENSOR: ");
-  Serial.println(sSensorActualValue);
+  //Serial.print("VAlOR DO SENSOR: ");
+  //Serial.println(sSensorActualValue);
   
-  if(sSensorActualValue >= metade) {
+  if(sSensorActualValue >= metade && sSensorActualValue < sSensorLimit) {
     //manda avisando que está próximo do valor  
-    //meetAndroid.send(HALF_VALUE);
+    meetAndroid.send("LOG: Já está na metade do valor");
+    meetAndroid.send("HALF_VALUE");
     //chama função que alerta no Arduino
     alterarLedsArduino(HALF_VALUE);
   } else if (sSensorActualValue >= sSensorLimit) {
     //manda avisando que o valor ultrapassou o limite
-    //meetAndroid.send(EXCEEDED_VALUE);
+    meetAndroid.send("LOG: Ultrapassou o limite");
+    meetAndroid.send("EXCEEDED_VALUE");
     //chama função que alerta no Arduino
     alterarLedsArduino(EXCEEDED_VALUE);
   }
@@ -93,18 +109,18 @@ void verificaLimite() {
 
 
 void pararMedicao () {
-  //meetAndroid.send(STOP);
+  meetAndroid.send("STOP");
   //pensar se precisa deste setup aqui
   delay(3000);
   setup();
 }
 
 void inicializarLedsArduino () {
-  Serial.println("Iniciar leds");
+  meetAndroid.send("LOG: Inicializar Leds");
 }
 
 void alterarLedsArduino (String valor) {
-  Serial.println("Alterar valor dos LEDS" + valor);
+  meetAndroid.send("LOG: Alterar Leds Arduino");
 }
 
 
