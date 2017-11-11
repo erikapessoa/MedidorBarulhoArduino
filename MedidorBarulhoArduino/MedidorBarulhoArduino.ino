78c4a2d755e74cf1da30d3974f8fe2acbff2105a
Erika Pessôa <erika.pessoa@gmail.com>
2017-11-11 17:14:01 -0300

    Última versão

--- MedidorBarulhoArduino/MedidorBarulhoArduino.ino
+++ MedidorBarulhoArduino/MedidorBarulhoArduino.ino
@@ -1,151 +1,192 @@
+#include <LedControl.h>
 #include <MeetAndroid.h>
+
+
 MeetAndroid meetAndroid;
 
+
+LedControl ledControl = LedControl(12,10,11,1);
+
 int sSensorAnalPin = A15; //porta analógica ligado ao sensor de som
+int sSensorDigitalPin = 3; //porta analógica ligado ao sensor de som
 int sSensorActualValue = 0;  //variável que guarda o valor que o sensor de som analógico esta captando
+int sSensorActualValueAndroid = 0;  //variável que guarda o valor que o sensor de som analógico esta captando convertido para Android
 int sSensorLimit = 0; //valor que define o limite de barulho que pode ser feito no ambiente
-int sSensorTimeMesuring = 0; //indica o tempo que o sensor de som irá ficar medindo o som ambiente
-int androidValues[] = {0,0}; //o primeiro valor é o limite e o segundo o tempo de medição
+//int sSensorTimeMesuring = 0; //indica o tempo que o sensor de som irá ficar medindo o som ambiente
+//int androidValues[] = {0,0}; //o primeiro valor é o limite e o segundo o tempo de medição
 //int sSensorCalibration = 0; //valor que define a calibração do microfone
-const String HALF_VALUE = "HALF_VALUE";
-const String EXCEEDED_VALUE = "EXCEEDED_VALUE";
-const String FINISH = "FINISH";
-const String STOP = "STOP";
+
+const int VALOR_MIN_ARDUINO = 530;
+const int VALOR_MAX_ARDUINO = 570;
+const int VALOR_MIN_ANDROID = 0;
+const int VALOR_MAX_ANDROID = 100;
 
 
-void setup()
-{
-  //pinMode(ledPin,OUTPUT);
+// carinha feliz
+byte happySmile[8]= {B00111100,B01000010,B10100101,B10000001,B10100101,B10011001,B01000010,B00111100};
+// carinha neutra
+byte neutralSmile[8]={B00111100, B01000010,B10100101,B10000001,B10111101,B10000001,B01000010,B00111100};
+// carinha triste
+byte sadSmile[8]= {B00111100,B01000010,B10100101,B10000001,B10011001,B10100101,B01000010,B00111100};
+
+
+
+bool MODE_MESURING = false;
+
+const int BELLOW_VALUE = 0;
+const int HALF_VALUE = 1;
+const int EXCEEDED_VALUE = 2;
+
+void setup() {
   
   Serial.begin(9600);
-  //inicializando as variáveis. Importante para toda vez que começar uma nova medição.
-  sSensorActualValue = 0;
-  sSensorLimit = 0;
-  sSensorTimeMesuring = 0;
-  //sSensorCalibration = 0; 
-  androidValues[0] = 0;
-  androidValues[1] = 0;
+
+  /*
+   The MAX7219 is in power-saving mode on startup, we have to do a wakeup call
+   */
+  ledControl.shutdown(0,false);
+  /* Set the brightness to a medium values */
+  ledControl.setIntensity(0,8);
+  /* and clear the display */
+  ledControl.clearDisplay(0);
+
   //Registrando a função que será chamada pelo Android para iniciar a medição.
   meetAndroid.registerFunction(iniciarMedicao, 'I');
   //Registrando a função que será chamada pelo Android para parar a medição antes do tempo acabar.
   meetAndroid.registerFunction(pararMedicao, 'P');
-  inicializarLedsArduino();
+
+  pinMode(sSensorDigitalPin,INPUT);
   
 }
 
 void loop(){
   meetAndroid.receive();
+
+  if(MODE_MESURING) {
+    mesuringAmbientSound();
+  }
+  
 }
 
 void iniciarMedicao() {
 
-  //Serial.println("Entrou em iniciarMedicao!!!");
-  meetAndroid.send("LOG: Entrou em iniciarMedicao!!!");
-  
-  long tempoMiliSegundos = 60000;
-  int segundos = 0;
-  
-  setup();
+  /*
+   The MAX7219 is in power-saving mode on startup, we have to do a wakeup call
+   */
+  ledControl.shutdown(0,false);
+  /* Set the brightness to a medium values */
+  ledControl.setIntensity(0,8);
+  /* and clear the display */
+  ledControl.clearDisplay(0);
 
-  meetAndroid.getIntValues(androidValues);
-  sSensorLimit = androidValues[0];
-  sSensorTimeMesuring = androidValues[1];
+  //inicializando as variáveis. Importante para toda vez que começar uma nova medição.
+  sSensorActualValue = 0;
+  //sSensorCalibration = 0; 
 
-  meetAndroid.send("LOG: sSensorLimit:  ");
-  meetAndroid.send(sSensorLimit);
-  meetAndroid.send("LOG: sSensorTimeMesuring:  ");
-  meetAndroid.send(sSensorTimeMesuring);
-  
-  tempoMiliSegundos = sSensorTimeMesuring * 60000; //o valor do tempo vem em minutos
+  inicializarLedsArduino();
 
-  
-  meetAndroid.send("LOG: Tempo em milisegundos:  ");
-  meetAndroid.send(tempoMiliSegundos);
-  
-  for (long i = 0; i <= tempoMiliSegundos; i++) {
-    segundos = i % 1000;
-    
-    if (segundos == 0) { //para ler apenas a cada segundo
-      //Serial.print("SEGUNDO: ");
-      //Serial.println(segundos);
-      sSensorActualValue = analogRead(sSensorAnalPin);  
-      verificaLimite();
-    }
-    
-    //meetAndroid.send("LOG: Valor de i =  ");
-    //meetAndroid.send(i);
-  }
-  
-  //envia mensagem para o Android avisando que acabou a medição
-  meetAndroid.send("FINISH");
-  //Serial.print("TERMINOU!!!");
-  
+  sSensorLimit = meetAndroid.getInt();  //valor que vem do app android
+
+  MODE_MESURING = true;
+
 }
 
+
+void mesuringAmbientSound() {
+
+  sSensorActualValue = analogRead(sSensorAnalPin);  
+
+  if (sSensorActualValue < VALOR_MIN_ARDUINO)
+      sSensorActualValue = VALOR_MIN_ANDROID;
+  else if (sSensorActualValue >= VALOR_MIN_ARDUINO && sSensorActualValue <= VALOR_MAX_ARDUINO)
+      sSensorActualValue = sSensorActualValue - VALOR_MIN_ARDUINO;
+  else
+      sSensorActualValue = VALOR_MAX_ANDROID;
+  
+  verificaLimite();
+  delay(2000); //verificar o som a cada 5 segundos
+
+}
 
 void verificaLimite() {
 
   int metade = sSensorLimit / 2;
 
-  meetAndroid.send("LOG: Leitura atual do sensor: ");
-  meetAndroid.send(sSensorActualValue); //envia para o android a leitura atual do sensor
-  //Serial.print("VAlOR DO SENSOR: ");
-  //Serial.println(sSensorActualValue);
+  sSensorActualValueAndroid = (VALOR_MAX_ANDROID * sSensorActualValue) / (VALOR_MAX_ARDUINO - VALOR_MIN_ARDUINO) ;
   
-  if(sSensorActualValue >= metade && sSensorActualValue < sSensorLimit) {
-    //manda avisando que está próximo do valor  
-    meetAndroid.send("LOG: Já está na metade do valor");
-    meetAndroid.send("HALF_VALUE");
+  if(sSensorActualValueAndroid < metade) {
+    //chama função que alerta no Arduino
+    alterarLedsArduino(BELLOW_VALUE);
+  } else if(sSensorActualValueAndroid >= metade && sSensorActualValueAndroid < sSensorLimit) {
     //chama função que alerta no Arduino
     alterarLedsArduino(HALF_VALUE);
-  } else if (sSensorActualValue >= sSensorLimit) {
-    //manda avisando que o valor ultrapassou o limite
-    meetAndroid.send("LOG: Ultrapassou o limite");
-    meetAndroid.send("EXCEEDED_VALUE");
+  } else if (sSensorActualValueAndroid >= sSensorLimit) {
     //chama função que alerta no Arduino
     alterarLedsArduino(EXCEEDED_VALUE);
   }
+
+  meetAndroid.send(sSensorActualValueAndroid); //envia para o android a leitura atual do sensor
+
 }
 
 
 void pararMedicao () {
-  meetAndroid.send("STOP");
-  //pensar se precisa deste setup aqui
-  delay(3000);
-  setup();
+
+  MODE_MESURING = false;
+
+  /* limpa o display */
+  ledControl.clearDisplay(0);
+  ledControl.shutdown(0,true);
+  
 }
 
 void inicializarLedsArduino () {
-  meetAndroid.send("LOG: Inicializar Leds");
-}
-
-void alterarLedsArduino (String valor) {
-  meetAndroid.send("LOG: Alterar Leds Arduino");
-}
-
-
-/*
- 
-
-int Led = 13;//define LED port
-int buttonpin = 3; //define switch port
-int  val;//define digital variable val
-
-void  setup()
-{
-  pinMode(Led,OUTPUT);//define LED as a output port
-  pinMode(buttonpin,INPUT);//define switch as a output port
-}
-
-void  loop()
-{ 
-  val = digitalRead(buttonpin);//read the value of the digital interface 3 assigned to val 
+  desenharSorrisoFeliz();
   
-  if(val == HIGH) {  //when the switch sensor have signal, LED blink
-    digitalWrite(Led,HIGH);
-  } else {
-    digitalWrite(Led,LOW);
+}
+
+void alterarLedsArduino (int valor) {
+  if (valor == BELLOW_VALUE) {//abaixo da metade do valor
+    desenharSorrisoFeliz();
+  } if(valor == HALF_VALUE) { //chegou na metade do valor
+    desenharSorrisoNeutro();
+  } else if(valor == EXCEEDED_VALUE) { //passou do valor
+    desenharSorrisoTriste();
   }
 }
 
-*/
+
+void desenharSorrisoFeliz() {
+  ledControl.setRow(0,0,happySmile[0]);
+  ledControl.setRow(0,1,happySmile[1]);
+  ledControl.setRow(0,2,happySmile[2]);
+  ledControl.setRow(0,3,happySmile[3]);
+  ledControl.setRow(0,4,happySmile[4]);
+  ledControl.setRow(0,5,happySmile[5]);
+  ledControl.setRow(0,6,happySmile[6]);
+  ledControl.setRow(0,7,happySmile[7]);
+}
+
+void desenharSorrisoNeutro() {
+  ledControl.setRow(0,0,neutralSmile[0]);
+  ledControl.setRow(0,1,neutralSmile[1]);
+  ledControl.setRow(0,2,neutralSmile[2]);
+  ledControl.setRow(0,3,neutralSmile[3]);
+  ledControl.setRow(0,4,neutralSmile[4]);
+  ledControl.setRow(0,5,neutralSmile[5]);
+  ledControl.setRow(0,6,neutralSmile[6]);
+  ledControl.setRow(0,7,neutralSmile[7]);
+}
+
+void desenharSorrisoTriste() {
+  ledControl.setRow(0,0,sadSmile[0]);
+  ledControl.setRow(0,1,sadSmile[1]);
+  ledControl.setRow(0,2,sadSmile[2]);
+  ledControl.setRow(0,3,sadSmile[3]);
+  ledControl.setRow(0,4,sadSmile[4]);
+  ledControl.setRow(0,5,sadSmile[5]);
+  ledControl.setRow(0,6,sadSmile[6]);
+  ledControl.setRow(0,7,sadSmile[7]);
+}
+
+
